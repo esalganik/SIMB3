b0fbf28f7a9b634fdb3a2d2576aaa834945b145a
Evgenii Salganik <salganikea@gmail.com>
2025-03-20 00:01:13 +0100

    New buoys added from Cryosphere Innovation

--- simb3_import.m
+++ simb3_import.m
@@ -1,8 +1,8 @@
-%% Dataset import
+%% Dataset import (CRREL IMB 2005-2015)
 close all; clear; clc; 
 ncvars =  {'time','lat','lon','z','hi','hs','sur','int','bot','T','hi_west','hs_west','sur_west','int_west','bot_west'};
-projectdir = 'C:\Users\evgenii.salganik\Documents\MATLAB\SIMB3';
-dinfo = dir( fullfile(projectdir, '*.nc') );
+projectdir = 'C:\Users\Evgenii\Documents\MATLAB\Cryosphere Innovation';
+dinfo = dir( fullfile(projectdir, '*updated.nc') );
 num_files = length(dinfo);
 filenames = fullfile( projectdir, {dinfo.name} );
 label = {dinfo.name};
@@ -27,7 +27,7 @@ clearvars ncvars projectdir dinfo filenames num_files this_file K time t_0
 
 %%
 figure
-tile = tiledlayout(3,2); tile.TileSpacing = 'compact'; tile.Padding = 'compact';
+tile = tiledlayout(3,2); tile.TileSpacing = 'compact'; tile.Padding = 'none';
 nexttile([1 2])
 load('batlow50.mat'); 
 for i = 1:length(t)
@@ -77,7 +77,7 @@ clearvars ax hXLabel hYLabel tile N N_pcolor ax batlowW x y xl yl
 
 %% Example of temperature measuments
 figure
-i = 10; % selected buoy
+i = 41; % selected buoy
 dTdz = diff(T{i},1,2);
 range = -20:2:20;
 contourf(datenum(t{i}),z{i}(1:end-1),dTdz'*10,range,'-','ShowText','off','LabelSpacing',400,'edgecolor','none'); hold on
@@ -88,5 +88,59 @@ hYLabel = ylabel('Depth (m)'); set([hYLabel gca],'FontSize',7,'FontWeight','norm
 hBar1 = colorbar; ylabel(hBar1,'Vertical temp. grad. (°C/m)','FontSize',7);
 name=convertStringsToChars(label{i}); title(sprintf('Vert. temp. gradient, %s',name(1:5)),'FontSize',8,'FontWeight','normal');
 leg = legend('','West (2020)','','Updated','box','on','NumColumns',1); set(leg,'FontSize',6,'Location','northwest','orientation','horizontal'); leg.ItemTokenSize = [30*0.5,18*0.5];
+ylim([round(min(int{i}),1)-0.2 round(max(sur{i}),1)+0.1]);
+datetick('x','mmm','keepticks','keeplimits'); xtickangle(0);
+
+%% Dataset import (CRREL IMBs 2005-2015, 2019-2024)
+close all; clear; clc; 
+ncvars =  {'time','lat','lon','z','hi','hs','sur','int','bot','T'};
+projectdir = 'C:\Users\Evgenii\Documents\MATLAB\Cryosphere Innovation';
+dinfo = dir( fullfile(projectdir, '*.nc') );
+num_files = length(dinfo);
+filenames = fullfile( projectdir, {dinfo.name} );
+label = {dinfo.name};
+time = cell(num_files,1); hi = time; hs = time; hi_west = time; hs_west = time; T = time;
+for K = 1 : num_files
+  this_file = filenames{K};
+  label{K} = label{K}(1:end-3);
+  time{K} = ncread(this_file, ncvars{1}); % days since 1978-09-01
+  t_0 = (datetime('01-Sep-1978 00:00')); t{K} = t_0 + days(time{K});
+  z{K} = ncread(this_file, ncvars{4});
+  hi{K} = ncread(this_file, ncvars{5});
+  hs{K} = ncread(this_file, ncvars{6});
+  sur{K} = ncread(this_file, ncvars{7});
+  int{K} = ncread(this_file, ncvars{8});
+  T{K} = ncread(this_file, ncvars{10});
+end
+clearvars ncvars projectdir dinfo filenames num_files this_file K time t_0
+
+%% Quicklook
+figure
+tile = tiledlayout(1,2); tile.TileSpacing = 'compact'; tile.Padding = 'none';
+nexttile
+load('batlow100.mat'); 
+for i = 1:length(t)
+    plot(t{i},hs{i},'color',batlow100(i,:)); hold on
+end
+hYLabel = ylabel('Snow depth (m)'); set([ hYLabel gca],'FontSize',8,'FontWeight','normal');
+nexttile
+for i = 1:length(t)
+    plot(t{i},hi{i},'color',batlow100(i,:)); hold on
+end
+hYLabel = ylabel('Ice thickness (m)'); set([ hYLabel gca],'FontSize',8,'FontWeight','normal');
+leg = legend(label,'box','off','NumColumns',2); set(leg,'FontSize',6,'Location','bestoutside','orientation','horizontal'); leg.ItemTokenSize = [30*0.33,18*0.33];
+
+%% Example of temperature measuments
+figure
+i = 27; % selected buoy
+dTdz = diff(T{i},1,2);
+range = -20:2:20;
+contourf(datenum(t{i}),z{i}(1:end-1),dTdz'*10,range,'-','ShowText','off','LabelSpacing',400,'edgecolor','none'); hold on
+plot(datenum(t{i}),sur{i},'b:','LineWidth',3); plot(datenum(t{i}),int{i},'b:','LineWidth',3); % Reprocessed interfaces
+load('batlow.mat'); colormap(batlow); % colormap(parula);
+hYLabel = ylabel('Depth (m)'); set([hYLabel gca],'FontSize',7,'FontWeight','normal');
+hBar1 = colorbar; ylabel(hBar1,'Vertical temp. grad. (°C/m)','FontSize',7);
+name=convertStringsToChars(label{i}); title(sprintf('Vert. temp. gradient, %s',name),'FontSize',8,'FontWeight','normal');
+leg = legend('','Updated','box','on','NumColumns',1); set(leg,'FontSize',6,'Location','northwest','orientation','horizontal'); leg.ItemTokenSize = [30*0.5,18*0.5];
 ylim([round(min(int{i}),1)-0.2 round(max(sur{i}),1)+0.1]);
 datetick('x','mmm','keepticks','keeplimits'); xtickangle(0);
